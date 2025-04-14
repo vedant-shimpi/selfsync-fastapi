2b7d21f73f1e994bd2e4bfec707286767e2efc9d
tejas sapkal <tejas.sapkal@digikore.com>
2025-04-14 18:53:40 +0530

    get-position-title-list api

---
+++ assessment/position_role.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from database import users_collection, position_collection
+from common.auth import get_current_user
+
+
+router = APIRouter()
+
+
+@router.get("/get-position-title-list", response_model=dict)
+async def get_position_list(curr_hr: dict = Depends(get_current_user)):
+    try:
+        hr_users_document = await users_collection.find_one({"email": curr_hr["email"]})
+        if not hr_users_document:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid token or user not found.")
+        
+        positions_cursor_list = [doc["position_title"] async for doc in position_collection.find({"user_id": hr_users_document["_id"]})]
+
+        if not positions_cursor_list:
+            return {"success":False, "data":[], "message":"Data not found."}
+        
+        return {"success":True, "data":positions_cursor_list, "message":"Data fetch successfully."}
+    except Exception as ex:
+        return {"success":False, "data":[], "message":str(ex)}
+    
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from business_logic.add_assessment import router as add_assessment_router
 from business_logic.ip_data import router as ip_data_router
 from business_logic.contact import router as contact_router
 from assessment.add_candidate import router as add_candidate_router
+from assessment.position_role import router as position_role_router
 
 
 
@@ -42,6 +43,7 @@ app.include_router(add_assessment_router, tags=["add_assessment"])
 app.include_router(ip_data_router, tags=["ip_data"])
 app.include_router(contact_router, tags=["contact"])
 app.include_router(add_candidate_router, tags=["add_candidate"])
+app.include_router(position_role_router, tags=["position_role"])
 
 
 
