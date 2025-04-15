727ea32d4c146ce0495b9615f3a4d371fffce0da
poojumore <pooja.more@digikore.com>
2025-04-15 18:36:36 +0530

    for send email as per contact and demo data submit

--- business_logic/contact.py
+++ business_logic/contact.py
@@ -4,6 +4,8 @@ from pymongo.errors import DuplicateKeyError
 from database import get_db
 from schemas import CreateContact
 from common.auth import get_current_user
+from business_logic.email import send_html_email
+import os
 
 router = APIRouter()
 
@@ -11,12 +13,51 @@ router = APIRouter()
 async def add_contact(contact: CreateContact, db=Depends(get_db)):
     contact_collection = db["contact"]
 
+    # Ensure unique contact_id
     contact_collection.create_index("contact_id", unique=True)
 
     try:
         contact_data = contact.dict()
-        contact_data["contact_id"] = str(contact_data["contact_id"]) 
+        contact_data["contact_id"] = str(contact_data["contact_id"])
         contact_collection.insert_one(contact_data)
-        return {"message": "Contact added successfully"}
+
+        # Decide the template based on contact_us_by
+        if contact.contact_us_by == "contact":
+            template = "login_info.html"
+        elif contact.contact_us_by == "demo":
+            template = "signup_otp.html"
+        else:
+            template = "default.html" 
+
+        send_html_email(
+            subject="Your contact info was submitted successfully",
+            recipient=contact.email,
+            template_name=template,
+            context={"first_name": contact.first_name.capitalize()}
+        )
+        
+        return {"message": "Your data has been successfully submitted."}
+
     except DuplicateKeyError:
         raise HTTPException(status_code=400, detail="Contact with this ID already exists")
+
+
+# @router.post("/add_contact")
+# async def add_contact(contact: CreateContact, db=Depends(get_db)):
+#     contact_collection = db["contact"]
+
+#     # Ensure unique contact_id
+#     contact_collection.create_index("contact_id", unique=True)
+
+#     try:
+#         contact_data = contact.dict()
+#         contact_data["contact_id"] = str(contact_data["contact_id"])
+#         contact_collection.insert_one(contact_data)
+
+    
+#         send_html_email(subject="Your contact info was submitted successfully", recipient= contact.email, template_name= "signup_otp.html", context= {"first_name":contact.first_name.capitalize()})
+#         return {"message": "Your data has been successfully submitted."}
+
+#     except DuplicateKeyError:
+#         raise HTTPException(status_code=400, detail="Contact with this ID already exists")
+    
