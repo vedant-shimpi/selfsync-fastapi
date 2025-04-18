076419b34590e660989d0881fa48f4f37f20d234
shilpakatore <shilpa.katore@digikore.com>
2025-04-18 16:48:54 +0530

    type cast str to int

--- common/constants.py
+++ common/constants.py
@@ -6,7 +6,7 @@ load_dotenv()
 # JWT
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "2b12PMeu6WtkGehQNX2OUaF1e1lbkePgdxhowD47XqlaUvnKIJ37fZu")
 ALGORITHM = os.getenv("JWT_ALGORITHM", "HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)  # 1440 minutes means 24 hours (1 day)
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))  # 1440 minutes means 24 hours (1 day)
 
 # Candidate status 
 CANDIDATE_STATUS = {
